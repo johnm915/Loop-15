2e4c4711638549fef57062c9cd4899be6cd1a64b
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-29 12:38:39 -0600

    Add project files.

---
+++ Loop 15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Loop_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i <= 15; i++)
+            Console.WriteLine(i);
+        }
+    }
+}
